f95baac8ef2d39942afc58ca1e78fbdf7c6e29ad
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-07 16:43:41 +0100

    aggiunta chiusura menu

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 //seleziona icona hamburger tramite assegnazione variabile
 const hamburgerMenu = document.getElementById ('hamburger-menu');
-
+//seleziona menu tramite assegnazione variabile
 const rightMenu = document.getElementById ('right-menu');
+//seleziona icona chiusura menu tramite assegnazione variabile
+const closeIcon = document.getElementById ('close-icon');
+
 
 //aggiungi EventListener al click dell'icona hamburger per aprire menu
 hamburgerMenu.addEventListener('click', function () {
     rightMenu.style.display = 'block';
-})
+});
+
+closeIcon.addEventListener('click', function () {
+    rightMenu.style.display = 'none';
+});
